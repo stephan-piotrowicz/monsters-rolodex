4a8decce1f8b7f9e91bad26288157f1d002e2386
Stephan Piotrowicz <stephan.piotrowicz@skipthedishes.ca>
2020-06-23 14:19:59 -0500

    Updates

---
+++ precache-manifest.53ea12f408d696f95ec9b4b05e55cc81.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b57adaf18b54b2ded26f03a3cd65587",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "89fec1c6f691a192c57f",
+    "url": "/monsters-rolodex/static/css/main.ce8e9861.chunk.css"
+  },
+  {
+    "revision": "12240f73676d8f2fbd85",
+    "url": "/monsters-rolodex/static/js/2.a3c82928.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.a3c82928.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89fec1c6f691a192c57f",
+    "url": "/monsters-rolodex/static/js/main.05c05513.chunk.js"
+  },
+  {
+    "revision": "6062e4a584c29c6e0372",
+    "url": "/monsters-rolodex/static/js/runtime-main.e3d252a9.js"
+  }
+]);
